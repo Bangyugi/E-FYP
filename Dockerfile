c93a34e955f46c0b2e395f86679e9cc76bf0edbd
Bangyugi <bangtranvan08@gmail.com>
2025-03-10 05:53:02 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Inspiron"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/java/com/bangvan/efyp/EFypApplication.java
@@ -0,0 +1,13 @@
+package com.bangvan.efyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFypApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFypApplication.class, args);
+    }
+
+}
