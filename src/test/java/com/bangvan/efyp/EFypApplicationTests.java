de70bbe4d27505276234670f32af9ab03e05fbf1
Bangyugi <bangtranvan08@gmail.com>
2025-03-31 19:31:11 +0700

    base-setup

--- src/test/java/com/bangvan/efyp/EFypApplicationTests.java
+++ src/test/java/com/bangvan/efyp/EFypApplicationTests.java
@@ -1,7 +1,5 @@
 package com.bangvan.efyp;
 
-import com.bangvan.efyp.controller.AuthenticationController;
-import com.bangvan.efyp.controller.UserController;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,15 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EFypApplicationTests {
 
-    @InjectMocks
-    private AuthenticationController authenticationController;
 
-    @InjectMocks
-    private UserController userController;
 
     @Test
     void contextLoads() {
-        Assertions.assertNotNull(authenticationController);
-        Assertions.assertNotNull(userController);
+
     }
 }
